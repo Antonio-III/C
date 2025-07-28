8fa666a293dceaa139ce30f86f5fc1b033ca6693
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-07-28 16:10:23 +0400

    Update: triangular_seq_sum.c

---
+++ Scripts/triangular_seq_sum.c
@@ -0,0 +1,33 @@
+// Calculates the sum of the Triangular number sequence up to the nth term (inclusive).
+// The module includes iterative and recursive solutions.
+#include <stdio.h>
+
+int t_itr(int n);
+int t_rec(int n);
+
+int main(int argc, char *argv[]){
+	int n;
+
+	printf("What is n? \n");
+	scanf("%d", &n);
+
+	printf("recursive: n=%d  Sum=%d \n", n, t_rec(n));
+	printf("iterative: n=%d  Sum=%d \n", n, t_itr(n));
+}
+
+int t_itr(int n){
+	int tri = 0;
+	for (int inc = 1; inc <= n; inc++){
+		tri+= inc;
+	}
+
+	return tri;
+}
+
+int t_rec(int n){
+	if (n <= 0){
+		return 0;
+	} else {
+		return n + t_rec(n-1);
+	}
+}
