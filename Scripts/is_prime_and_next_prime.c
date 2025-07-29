bd3a170990c53ec964990990a137789a0a9eec67
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-07-29 14:25:34 +0400

    Update: is_prime....c

--- Scripts/is_prime_and_next_prime.c
+++ Scripts/is_prime_and_next_prime.c
@@ -1,5 +1,4 @@
-// A program that calculates how many days has it been since Dec 31.
-
+// Check if an inputted number is prime and output the result. Then, output the next prime number.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -7,18 +6,18 @@
 bool isprime(int n);
 int nextprime(int old_prime);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int n;
     
     printf("Enter a number: ");
     
-    if (scanf("%d", &n) !=1){
+    if (scanf("%d", &n) != 1) {
         printf("No number entered!\n");
     }
 
-    if (isprime(n)){
+    if (isprime(n)) {
         printf("%d is a prime.\n", n);
-    } else{
+    } else {
         printf("%d is not a prime.\n", n);
     }
 
@@ -27,9 +26,9 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-bool isprime(int n){
+bool isprime(int n) {
     // Prime numbers are by definition greater than 1.
-    if (n <= 1){
+    if (n <= 1) {
         return false;
     }
 
@@ -37,19 +36,19 @@ bool isprime(int n){
     // A number `n` is a prime if any preceeding number `divisor` (2 to sqrt(n) inclusive) squared is at most equal to `n` and can divide `n`.
     // For instance, `n = 9`.`divisor` iteratively takes a value in the range [2, 3], and divides `n`. If a valid divisor is found (i.e. no remainders), the loop terminates prematurely. In the example, `divisor` takes in the value of 2, but 2 cannot fully divide 9, so `divisor` takes in the next value. 3 is a valid divisor for 9, so the loop terminates and returns `false`. 
     // Had `divisor` taken in a value outside the range, `n` would be a prime.
-    for (int divisor = 2; divisor*divisor <= n; divisor++){
-        if (!(n%divisor)){
+    for (int divisor = 2; divisor*divisor <= n; divisor++) {
+        if (!(n % divisor)) {
             return false; 
-            }
+        }
     }
     
     return true;
 }
 
-int nextprime(int old_prime){
-    int next_prime=old_prime+1;
+int nextprime(int old_prime) {
+    int next_prime = old_prime + 1;
 
-    while(!isprime(next_prime)){
+    while(!isprime(next_prime)) {
         next_prime++;
     }
     
