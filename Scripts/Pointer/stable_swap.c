7fbe3e33609d80b7f310d1df4a90a791223c3af8
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-09-01 09:45:31 +0400

    Add stable_swap.c

---
+++ Scripts/Pointer/stable_swap.c
@@ -0,0 +1,19 @@
+// stable_swap is a version of the swap function in quicksort. 
+// Its purpose is to make quicksort a stable sorting algorithm. 
+// Its runtime degenerates to N from constant.
+// This function is untested so it is not clear if this is the correct implementation for all swap cases in quicksort.
+typedef int flex_t;
+
+void stable_swap(flex_t A[], int fe, int next);
+
+void stable_swap(flex_t A[], int fe, int next) {
+    // T: n, S: 1
+    flex_t temp;
+    while (next > fe) {
+        temp = A[next];
+        A[next] = A[next-1];
+        A[next-1] = temp;
+        next -= 1;
+    }
+    return;
+}
