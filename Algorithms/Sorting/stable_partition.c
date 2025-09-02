2a722d8109dee500deeb5ecd762139916dba40c5
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-09-02 11:04:05 +0400

    Add stable_partition.c

---
+++ Algorithms/Sorting/stable_partition.c
@@ -0,0 +1,28 @@
+// This module is a modification of the partition function in quicksort.
+// Courtesy of ChatGPT.
+typedef flex_t;
+
+void stable_partition(flex_t A[], int n, flex_t *pivot, int *fe, int *fg);
+
+void stable_partition(flex_t A[], int n, flex_t *pivot, int *fe, int *fg) {
+    flex_t *L = malloc(n *sizeof(flex_t));
+    flex_t *E = malloc(n *sizeof(flex_t));
+    flex_t *R = malloc(n *sizeof(flex_t));
+    int l=0, e=0, r=0;
+
+    for (int i=0; i<n; i++) {
+        int outcome = cmp(&A[i], pivot);
+        if (outcome < 0) L[l++] = A[i];
+        else if (outcome > 0) R[r++] = A[i];
+        else E[e++] = A[i];
+    }
+
+    memcpy(A, L, l * sizeof(flex_t));
+    memcpy(A+l, E, e * sizeof(flex_t));
+    memcpy(A+l+e, R, r * sizeof(flex_t));
+
+    *fe = l;
+    *fg = l+e;
+
+    free(L); free(E); free(R);
+}
