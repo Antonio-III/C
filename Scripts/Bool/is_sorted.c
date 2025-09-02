5562de2a9572aeb8e6879f1351d170de3f71a722
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-09-02 12:02:01 +0400

    Update: convert indent to spaces

--- Scripts/Bool/is_sorted.c
+++ Scripts/Bool/is_sorted.c
@@ -11,30 +11,30 @@ int is_sorted(int A[], int n);
 void print_array(int A[], int n);
 
 int main(int argc, char *argv[]) {
-	int A[] = ARRAY;
-	
+    int A[] = ARRAY;
+    
     print_array(A, N);
-	if (is_sorted(A, N)) {
-		printf("is sorted.\n");
-	} else {
-		printf("is not sorted.\n");
-	}
-	return 0;
+    if (is_sorted(A, N)) {
+        printf("is sorted.\n");
+    } else {
+        printf("is not sorted.\n");
+    }
+    return 0;
 }
 
 int is_sorted(int A[], int n) {
-	// equivalent to `for n in A` in Python.
-	for (int i = 0; i < n-1; i++){
-		if (A[i] > A[i+1]) {
-			return !SORTED; 
-		}
-	}
-	return SORTED;
+    // equivalent to `for n in A` in Python.
+    for (int i = 0; i < n-1; i++){
+        if (A[i] > A[i+1]) {
+            return !SORTED; 
+        }
+    }
+    return SORTED;
 }
 
 void print_array(int A[], int n){
-	for (int i = 0; i < n-1; i++){
-		printf("%d ", A[i]);
-	}
-	return;
+    for (int i = 0; i < n-1; i++){
+        printf("%d ", A[i]);
+    }
+    return;
 }
