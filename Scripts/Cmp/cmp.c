17aef344a627ce36867f2dcd44c42ca63a94fd62
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-09-04 12:33:10 +0400

    Update: cmp

--- Scripts/Cmp/cmp.c
+++ Scripts/Cmp/cmp.c
@@ -7,7 +7,9 @@ flex_t cmp(flex_t *x1, flex_t *x2);
 
 flex_t 
 cmp(flex_t *x1, flex_t *x2) {
-    return (*x1 - *x2);
+    if (*x1 < *x2) return -1;
+    else if (*x1 > *x2) return 1;
+    else return 0;
 }
 
 int
