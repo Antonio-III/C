4d3771419c7ff84a7e7b6f36755f2c7837391d0a
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-09-04 13:08:06 +0400

    Add: quicksort_no_tail.c

--- Algorithms/Sorting/quicksort.c
+++ Algorithms/Sorting/quicksort.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAXVALS 11
+#define ARR {33, 12, 55, 33, 66, 61, 18, 16, 29, 33, 19}
 
 typedef int data_t;
 
 void quick_sort(data_t A[], int n);
 data_t choose_pivot(data_t A[], int n);
-void partition(data_t A[], int n, data_t pivot, int *first_eq, int *first_gt);
+void partition(data_t A[], int n, data_t *pivot, int *first_eq, int *first_gt);
 
 // imports
 void print_array(data_t A[], int n);
 data_t cmp(data_t *x1, data_t *x2);
 void swap(data_t *p1, data_t *p2);
 
-int
+int 
 main(int argc, char *argv[]) {
-    data_t numbers[MAXVALS] = 
-        {33, 12, 55, 33, 66, 61, 18, 16, 29, 33, 19};
+    data_t numbers[MAXVALS] = ARR;
     int nnumbs = MAXVALS;
     
+    srand(time(NULL));
     printf("Before:");
     print_array(numbers, nnumbs);
 
@@ -32,15 +35,20 @@ main(int argc, char *argv[]) {
 
 void
 quick_sort(data_t A[], int n) {
-    int first_eq, first_gt;
+    // T: O(n^2) worst, O(nlogn) average
+    // To get logn, our recursion depth must be logn at most while our partition remains n.
+    
     if (n <= 1) {
         return;	
     }
+
+    int first_eq, first_gt;
+
     // 1.
     data_t pivot = choose_pivot(A, n);
 
     // 2.
-    partition(A, n, pivot, &first_eq, &first_gt);
+    partition(A, n, &pivot, &first_eq, &first_gt);
 
     // 3.
     quick_sort(A, first_eq);
@@ -52,18 +60,18 @@ choose_pivot(data_t A[], int n) {
     // 4.
     return A[0];
 
-    // return &A[rand()%n];
-    // return &A[n/2];
+    // return A[rand()%n];
+    // return A[n/2];
 }
 
 void 
-partition(data_t A[], int n, data_t pivot, int *first_eq, int *first_gt) {
+partition(data_t A[], int n, data_t *pivot, int *first_eq, int *first_gt) {
+    // T: O(n)
 
-    // T: O(n) worst, O(log n) average
     int next = 0, fe = 0, fg = n, outcome;
 
     while (next < fg) {
-        if ((outcome = cmp(A+next, &pivot)) < 0) {
+        if ((outcome = cmp(A+next, pivot)) < 0) {
             swap(A+fe, A+next);
             fe += 1;
             next += 1;
@@ -75,10 +83,17 @@ partition(data_t A[], int n, data_t pivot, int *first_eq, int *first_gt) {
         }
     }
 
-    assert(0 <= fe);
+    assert(0 <= fe && fe < fg && fg <= n);
     *first_eq = fe;
     *first_gt = fg;
     return; 
+}
+
+data_t 
+cmp(data_t *x1, data_t *x2) {
+    if (*x1 < *x2) return -1;
+    else if (*x1 > *x2) return 1;
+    else return 0;
 }
 
 void 
@@ -87,12 +102,6 @@ print_array(data_t A[], int n) {
         printf(" %d", A[i]);
     }
     printf("\n");
-}
-
-
-data_t 
-cmp(data_t *x1, data_t *x2) {
-    return (*x1 - *x2);
 }
 
 void swap(data_t *p1, data_t *p2) {
