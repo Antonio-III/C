3a2ac03e0f2172181dcb28bfa44d0101b1aa2735
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-11-27 11:18:24 +0400

    Add: subset_sum.c

---
+++ Scripts/Bool/subset_sum.c
@@ -0,0 +1,34 @@
+// This script answers: "Is there a subset of A that sum to T?"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int subsetsum(int A[], int n, int k);
+
+int main(int argc, char *argv[]) {
+    int A[] = {
+    34, 38, 39, 43, 55, 66, 67, 84, 85, 91,
+    101, 117, 128, 138, 165, 168, 169, 182, 184, 186,
+    234, 238, 241, 276, 279, 288, 386, 387, 388, 389,
+    413, 444, 487, 513, 534, 535, 616, 722, 786, 787
+    };
+    // default states
+    int n = 20, k = 1000;
+
+    if (argc == 3) {
+        n = atoi(argv[1]);
+        k = atoi(argv[2]);
+    }
+
+    printf("n=%d, k=%d, subsetsum=%d\n", 
+    n, k, subsetsum(A, n, k));
+    
+    return 0;
+}
+
+int subsetsum(int A[], int n, int k) {
+    if (k == 0) return 1;
+    else if (n == 0) return 0;
+    
+    else return subsetsum(A, n-1, k-A[n-1]) || subsetsum(A, n-1, k);
+}
